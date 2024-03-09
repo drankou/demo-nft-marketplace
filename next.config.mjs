bc7c9c9a72a74b68fd893654dde540ce805632f4
Aliaksandr Drankou <drankovalexander@gmail.com>
2024-03-09 12:38:02 +0100

    Allow all domains for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["t.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
