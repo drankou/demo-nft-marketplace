3e928d13e4716763dcf41ecd1e0a4a974003d1c2
Aliaksandr Drankou <drankovalexander@gmail.com>
2024-03-10 16:39:36 +0100

    Fix navbar title width

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
   return (
     <header className="fixed -top-1 left-0 z-10 w-full">
       <div className="absolute left-0 top-0 flex w-full items-center justify-between bg-background/60 px-8 py-6 backdrop-blur-md">
-        <span className="w-[200px] text-nowrap text-2xl font-semibold">
+        <span className="text-nowrap text-2xl font-semibold">
           💎 Marketplace
         </span>
         <div className="flex w-full items-center justify-end gap-2">
