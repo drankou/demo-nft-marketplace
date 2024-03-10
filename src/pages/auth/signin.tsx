6548bb64152bd44c99e5c16de55322dc5cb9429d
Aliaksandr Drankou <drankovalexander@gmail.com>
2024-03-10 17:02:55 +0100

    Improve sign in page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,48 +1,46 @@
 import { LoginButton } from '@telegram-auth/react'
-import { getCsrfToken, signIn } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { GetServerSidePropsContext } from 'next/types'
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const csrfToken = await getCsrfToken(context)
   const botUsername = process.env.BOT_USERNAME
   return {
     props: {
-      csrfToken,
       botUsername,
     },
   }
 }
 
-const Signin = ({
-  botUsername,
-  csrfToken,
-}: {
-  botUsername: string
-  csrfToken?: string
-}) => {
+const Signin = ({ botUsername }: { botUsername: string }) => {
   const router = useRouter()
 
   const { error } = router.query
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      {error && (
-        <p className="p-4 text-red-500">
-          Oops! Something went wrong 😕 Try again.
-        </p>
-      )}
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <LoginButton
-        botUsername={botUsername}
-        requestAccess={null}
-        showAvatar={false}
-        widgetVersion={22}
-        onAuthCallback={(data) => {
-          signIn('telegram', { callbackUrl: '/' }, data as any)
-        }}
-      />
-    </div>
+    <>
+      <div className="absolute left-1/2 top-1/3 -translate-x-1/2">
+        <span className="text-nowrap text-4xl font-semibold">
+          💎 Marketplace
+        </span>
+      </div>
+      <div className="flex h-screen flex-col items-center justify-center">
+        {error && (
+          <p className="p-4 text-red-500">
+            Oops! Something went wrong 😕 Try again.
+          </p>
+        )}
+        <LoginButton
+          botUsername={botUsername}
+          requestAccess={null}
+          showAvatar={false}
+          widgetVersion={22}
+          onAuthCallback={(data) => {
+            signIn('telegram', { callbackUrl: '/' }, data as any)
+          }}
+        />
+      </div>
+    </>
   )
 }
 
